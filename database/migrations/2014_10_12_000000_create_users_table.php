281f0e142252eda4eade0921b505f1555956e11d
Samir Shrestha <theonlysamir@gmail.com>
2021-12-29 13:39:15 +0545

    user number added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('mobile')->nullable();
             $table->string('password');
             // Driver's only
             $table->double('latitude')->nullable();
