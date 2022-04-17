18a4885f618f524fe107c9eebcfab8c2dae2148e
Susmita <susa.limbu@gmail.com>
2022-04-17 22:52:27 +0545

    home page created

---
+++ resources/views/users/components/frontendfooter.blade.php
@@ -0,0 +1,33 @@
+<footer>
+<div class="row container mx-auto mt-4 py-5">
+    <div class="col-md-6">
+        <div class="row g-0">
+            <div class="col-3">
+                <h1>Logo</h1>
+            </div>
+            <div class="col-9">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem expedita a, necessitatibus, cum, ipsam pariatur nostrum nesciunt veritatis laborum vitae nobis. Inventore earum quia distin...<a href="#">Read more</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <p class="mb-0 fw-bold">Navigation</p>
+        <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Ride</li>
+        </ul>
+    </div>
+    <div class="col-md-3">
+        <p class="mb-0 fw-bold">Contact us</p>
+        <ul>
+            <li>example.gmail.com</li>
+            <li>98343763746</li>
+        </ul>
+    </div>
+</div>
+<div class="row container mx-auto text-center py-2 mt-2">
+    <small class="col-12">Copyright &copy; 2014-2021</small>
+</div>
+</footer>
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -7,12 +7,15 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- fontawesome --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
     <title>Hello, world!</title>
   </head>
   <body>
     @include('users.components.navbar')
     @yield('content')
+    @include('users.components.frontendfooter')
 
     <!-- Optional JavaScript; choose one of the two! -->
 
