82dee86ab709d81a811efdc016d190b0ac177067
Susmita <susa.limbu@gmail.com>
2022-04-18 22:56:29 +0545

    navbar adjusted

--- resources/views/users/components/nav.blade.php
+++ resources/views/users/components/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container py-1">
       <a class="navbar-brand fs-3 fw-bold" href="#">Logo</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
